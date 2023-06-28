c94da3119170ab616ca23c38f19ad99e09157c90
Mamed Khasiev <shuchenysh@gmail.com>
2023-06-28 21:29:31 +0300

    Update

---
+++ app/src/main/java/com/shuchenysh/monowallet/data/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.shuchenysh.monowallet.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LoginViewModel(application: Application): AndroidViewModel(application) {
+
+
+}
