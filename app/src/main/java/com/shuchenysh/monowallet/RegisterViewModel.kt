32df4771a724313442752bdac606190ef873db7f
Mamed Khasiev <shuchenysh@gmail.com>
2023-07-17 07:55:48 +0300

    add db and dependencies

--- app/src/main/java/com/shuchenysh/monowallet/RegisterViewModel.kt
+++ app/src/main/java/com/shuchenysh/monowallet/RegisterViewModel.kt
@@ -2,20 +2,16 @@ package com.shuchenysh.monowallet
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import com.shuchenysh.monowallet.data.UsersDatabase
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 class RegisterViewModel(application: Application) : AndroidViewModel(application) {
 
     private val db = UsersDatabase.getInstance(application)
-    private val compositeDisposable = CompositeDisposable()
 
     fun addUser(user: User) {
         db.usersDao().add(user)
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposable.dispose()
-    }
 }
